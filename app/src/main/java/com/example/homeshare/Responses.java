bff0f070872dc45ee7bdba5091c8c595fee492b4
emmacheung <ezcheung@usc.edu>
2022-10-23 13:19:40 -0700

    responses edits

--- app/src/main/java/com/example/homeshare/Responses.java
+++ app/src/main/java/com/example/homeshare/Responses.java
@@ -3,12 +3,62 @@ package com.example.homeshare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class Responses extends AppCompatActivity {
+
+    // variables for list view
+    private ListView responses;
+
+    ArrayList<String> responsesArrayList;
+
+    // database reference
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_responses);
+
+        // initializing variables for listviews.
+        responses = findViewById(R.id.responses);
+        // initializing our array list
+        responsesArrayList = new ArrayList<String>();
+
+        createResponseList();
+    }
+
+    private void createResponseList() {
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, responsesArrayList);
+
+        reference = FirebaseDatabase.getInstance().getReference();
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                textView.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                textView.setText("Error in retrieving your message!");
+                Log.w("SecondFragment", "Failed to read value.", error.toException());
+            }
+        });
     }
 }
