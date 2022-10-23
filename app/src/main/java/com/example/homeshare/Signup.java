1fc4b3969d71846c5f35b44e8968bb832858e319
lucindaq <lucinda@merl.us>
2022-10-23 15:35:49 -0700

    signup input validation and input types implemented

--- app/src/main/java/com/example/homeshare/Signup.java
+++ app/src/main/java/com/example/homeshare/Signup.java
@@ -3,6 +3,9 @@ package com.example.homeshare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Signup extends AppCompatActivity {
 
@@ -10,5 +13,27 @@ public class Signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        TextView submitBtn = (TextView) findViewById(R.id.signUpButton);
+        submitBtn.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    public void onClick(View view)
+                    {
+                        String fName = ((EditText) findViewById(R.id.firstName)).getText().toString();
+                        String lName = ((EditText) findViewById(R.id.lastName)).getText().toString();
+                        String email = ((EditText) findViewById(R.id.signUpEmail)).getText().toString();
+                        String pass = ((EditText) findViewById(R.id.signUpPassword)).getText().toString();
+                        String confPass = ((EditText) findViewById(R.id.confPassword)).getText().toString();
+                        TextView err = (TextView) findViewById(R.id.signUpErrMsg);
+                        if (fName.equals("") || lName.equals("") || email.equals("") || pass.equals("") || confPass.equals("")) {
+                            err.setText("Error: Please fill out all fields!");
+                        } else if (!pass.equals(confPass)) {
+                            err.setText("Error: Passwords don't match!");
+                        } else {
+                            err.setText("");
+                        }
+                    }
+                });
     }
 }
