1e991c2ef0c48aab79e8cfb05de278171c93be8d
emmacheung <ezcheung@usc.edu>
2022-10-23 11:45:41 -0700

    test-clear idea files

--- app/src/main/java/com/example/homeshare/MainActivity.java
+++ app/src/main/java/com/example/homeshare/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.homeshare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView loginButton = (TextView) findViewById(R.id.login);
+        loginButton.setOnClickListener(this::onClickLogin);
     }
+
+    public void onClickLogin(View view) {
+        Intent intent = new Intent(this, login.class);
+        startActivity(intent);
+        return;
+    }
+
 }
--- app/src/main/java/com/example/homeshare/login.java
+++ app/src/main/java/com/example/homeshare/login.java
@@ -2,7 +2,11 @@ package com.example.homeshare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        EditText editView = (EditText) findViewById(R.id.email);
+        String message = editView.getText().toString();
+
+        TextView loginButton = (TextView) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(this::onClickLogin);
+    }
+
+    public void onClickLogin(View view) {
+        Intent intent = new Intent(this, Home.class);
+        startActivity(intent);
+        return;
     }
 }
