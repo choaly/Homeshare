bfabb9b5af5722de9a2d30b3842b87211ce29c77
emmacheung <ezcheung@usc.edu>
2022-10-25 22:28:33 -0700

    response + response detail page

---
+++ app/src/main/java/com/example/homeshare/ResponseDetails.java
@@ -0,0 +1,14 @@
+package com.example.homeshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResponseDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_response_details);
+    }
+}
