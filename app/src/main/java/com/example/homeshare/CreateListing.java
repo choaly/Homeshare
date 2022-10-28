24ba216b66d4577edc6a7e18ca108649132f136e
lucindaq <lucinda@merl.us>
2022-10-28 14:22:18 -0700

    create listing added input checking, prep for add to db

--- app/src/main/java/com/example/homeshare/CreateListing.java
+++ app/src/main/java/com/example/homeshare/CreateListing.java
@@ -5,7 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CreateListing extends AppCompatActivity {
 
@@ -14,14 +19,69 @@ public class CreateListing extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_listing);
 
-        TextView postBtn = (TextView) findViewById(R.id.postListingButton);
+        TextView postBtn = findViewById(R.id.postListingButton);
         postBtn.setOnClickListener(this::handleCreateListing);
     }
 
     public void handleCreateListing(View view) {
-        // Implement create listing and add to db functionality, reroute to home, etc
-        Intent intent = new Intent(this, Home.class);
-        startActivity(intent);
-        return;
+        String listingTitle = ((TextView) findViewById(R.id.CLlistingTitle)).getText().toString();
+        String address = ((TextView) findViewById(R.id.CLlistingAddr)).getText().toString();
+        String leaseStartString = ((TextView) findViewById(R.id.CLleaseStart)).getText().toString();
+        String leaseEndString = ((TextView) findViewById(R.id.CLleaseEnd)).getText().toString();
+        String description = ((TextView) findViewById(R.id.CLlistingDescription)).getText().toString();
+        String pricePerMonthString = ((TextView) findViewById(R.id.CLpricePerMonth)).getText().toString();
+        String numSpotsAvailableString = ((TextView) findViewById(R.id.CLnumSpotsAvailable)).getText().toString();
+        int selectedGenderId = ((RadioGroup) findViewById(R.id.CLprefGenderRadioGroup)).getCheckedRadioButtonId();
+        String preferredGender = "";
+        if (selectedGenderId != -1) {
+            preferredGender = ((RadioButton) findViewById(selectedGenderId)).getText().toString();
+        }
+        String responseDeadlineString = ((TextView) findViewById(R.id.CLresponseDeadline)).getText().toString();
+        Date responseDeadline = null;
+        try {
+            responseDeadline = new SimpleDateFormat("dd/MM/yyyy").parse(leaseEndString);
+        } catch (java.text.ParseException err) {
+            err.printStackTrace();
+        }
+
+        boolean dateErr = false;
+        Date leaseStart = null;
+        try {
+            leaseStart = new SimpleDateFormat("dd/MM/yyyy").parse(leaseStartString);
+        } catch (java.text.ParseException err) {
+            err.printStackTrace();
+            dateErr = true;
+        }
+        Date leaseEnd = null;
+        try {
+            leaseEnd = new SimpleDateFormat("dd/MM/yyyy").parse(leaseEndString);
+        } catch (java.text.ParseException err) {
+            err.printStackTrace();
+            dateErr = true;
+        }
+
+        if (!dateErr && listingTitle.length() > 0 && address.length() > 0 && leaseStartString.length() > 0
+                && leaseEndString.length() > 0 && description.length() > 0 && pricePerMonthString.length() > 0
+                && numSpotsAvailableString.length() > 0 && preferredGender.length() > 0 && responseDeadlineString.length() > 0)
+        {
+            double pricePerMonth = Double.parseDouble(pricePerMonthString);
+            int numSpotsAvailable = Integer.parseInt(numSpotsAvailableString);
+
+            // Add all to db
+
+            Intent intent = new Intent(this, Home.class);
+            startActivity(intent);
+            return;
+        } else {
+            String msg = "";
+            if (dateErr) {
+                msg += "Date error";
+            }
+            msg += " Please fill in all fields";
+            TextView errMsg = findViewById(R.id.createListingErrMsg);
+            errMsg.setText(msg);
+        }
+
+
     }
 }
