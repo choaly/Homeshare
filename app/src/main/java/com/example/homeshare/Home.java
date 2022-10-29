63937ce85f8fde8e91f6a335c76bfd45ccd466f1
choaly <67077090+choaly@users.noreply.github.com>
2022-10-29 16:13:06 -0700

    worked on Account page and Nav Bar

--- app/src/main/java/com/example/homeshare/Home.java
+++ app/src/main/java/com/example/homeshare/Home.java
@@ -1,15 +1,53 @@
 package com.example.homeshare;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 
 import android.os.Bundle;
 
+import com.example.homeshare.databinding.ActivityHomeBinding;
+
 public class Home extends AppCompatActivity {
+
+    ActivityHomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+//        setContentView(R.layout.activity_home);
+        setContentView(binding.getRoot());
+        replaceFragment(new AccountFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()) {
+                case R.id.home:
+//                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.responses:
+//                    replaceFragment(new ResponseFragment());
+                    break;
+                case R.id.my_account:
+                    replaceFragment(new AccountFragment());
+                    break;
+            }
+
+            return true;
+
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+
     }
 
 
