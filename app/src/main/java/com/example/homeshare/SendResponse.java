896c1c4cebc1a637673a0cebce68bd9fe1636b3e
emmacheung <ezcheung@usc.edu>
2022-11-05 21:02:50 -0700

    create listings in db and make send response page

---
+++ app/src/main/java/com/example/homeshare/SendResponse.java
@@ -0,0 +1,14 @@
+package com.example.homeshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SendResponse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_response);
+    }
+}
