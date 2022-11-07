cf92ab1108953a01cfbf56aa12d230403bd5d231
lucindaq <lucinda@merl.us>
2022-11-07 09:19:39 -0800

    user registration working

--- app/src/main/java/com/example/homeshare/Signup.java
+++ app/src/main/java/com/example/homeshare/Signup.java
@@ -1,19 +1,30 @@
 package com.example.homeshare;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Signup extends AppCompatActivity {
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        auth = FirebaseAuth.getInstance();
 
         TextView submitBtn = (TextView) findViewById(R.id.signUpButton);
         submitBtn.setOnClickListener(this::onClickSignup);
@@ -35,9 +46,23 @@ public class Signup extends AppCompatActivity {
             err.setText("Error: Passwords don't match!");
         } else {
             err.setText("");
-            Intent intent = new Intent(this, Home.class);
-            startActivity(intent);
+            registerUser(email, pass);
         }
+    }
+
+    private void registerUser(String email, String pass) {
+        auth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(Signup.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(Signup.this, "Success, welcome to Homeshare!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(Signup.this, Home.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(Signup.this, "ERROR creating user", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     public void onClickBack(View view) {
